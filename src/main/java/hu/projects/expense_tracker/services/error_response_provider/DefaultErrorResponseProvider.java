c0339f631f31aab16ee17758328800feb91f353f
pisti1555 <pisti2319@gmail.com>
2025-12-14 13:09:30 +0100

    Refactor application configuration files. Add DefaultErrorResponseProvider.

---
+++ src/main/java/hu/projects/expense_tracker/services/error_response_provider/DefaultErrorResponseProvider.java
@@ -0,0 +1,15 @@
+package hu.projects.expense_tracker.services.error_response_provider;
+
+import hu.projects.expense_tracker.common.exceptions.ApiException;
+import hu.projects.expense_tracker.common.models.error_responses.ErrorResponse;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile({"!production", "!dev"})
+public class DefaultErrorResponseProvider implements EnvironmentBasedErrorResponseProvider {
+    @Override
+    public ErrorResponse convertException(ApiException exception, String customMessage) {
+        return new ProductionErrorResponseProvider().convertException(exception, customMessage);
+    }
+}
